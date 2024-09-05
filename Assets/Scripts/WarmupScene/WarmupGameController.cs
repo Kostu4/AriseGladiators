7540687bc1d9b429c4383236eff9a9a82eb73f4e
Kostu4 <webcorpgaming@gmail.com>
2024-09-05 20:31:48 +0300

    Update 9\5\24
    Fix some moments in GameScenes

--- Assets/Scripts/WarmupScene/WarmupGameController.cs
+++ Assets/Scripts/WarmupScene/WarmupGameController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Scripts.EnemyScript;
-using Scripts.PlayerScript;
 using UnityEngine.UI;
 
 namespace Scripts.WarmupScene
@@ -18,44 +14,45 @@ namespace Scripts.WarmupScene
         
         private bool isPaused = false;
         private int coinsCollected = 0;
-        [SerializeField] private int killCount = 0;
+        private int killCount = 0;
 
         protected override void Awake()
         {
             base.Awake();
-            timer = GetComponent<Timer>();
             timer.TimeOut.AddListener(TimerIsOut);
             endRoundCanvas.SetActive(false);
             Time.timeScale = isPaused ? 0 : 1;
         }
 
-        
         protected override void HandleEnemyDeath(IEnemy enemy)
         {
-            base.HandleEnemyDeath(enemy);
-            killCount++;
-            killCountText.text = killCount.ToString();
-            coinsCollected += enemy.EnemyCoins/2;
-            coinsCollectedText.text = coinsCollected.ToString();
-            base.HandleEnemyDeath(enemy);
+            if (!isPaused)
+            { 
+                base.HandleEnemyDeath(enemy);
+                killCount++;
+                killCountText.text = killCount.ToString();
+                coinsCollected += enemy.EnemyCoins/2;
+                coinsCollectedText.text = coinsCollected.ToString();
+            }
         }
 
         private void TimerIsOut()
         {
-            Debug.LogWarning("Timer is out Warmup");
             endRoundCanvas.SetActive(true);
             PauseGame();
-
+            PlayerPrefs.SetInt("coins", coinsCollected);
         }
 
         private void PauseGame()
         {
             isPaused = true;
+            Time.timeScale = 0;
         }
 
         private void ResumeGame()
         {
             isPaused = false;
+            Time.timeScale = 1;
         }
     }
 }
